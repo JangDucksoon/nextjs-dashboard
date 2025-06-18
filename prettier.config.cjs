7df9e3a8241935e851aea86b1f21ddb97bfbcc01
jds <smaa11271@wavus.co.kr>
2025-06-18 17:47:34 +0900

    git

---
+++ prettier.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+	arrowParens: 'always',
+	useTabs: true, // ← Tab 사용
+	tabWidth: 4, // ← 탭 크기 4칸
+	semi: true,
+	singleQuote: true,
+	trailingComma: 'none',
+	printWidth: 160,
+	endOfLine: 'auto',
+	proseWrap: 'preserve',
+	plugins: ['prettier-plugin-tailwindcss']
+};
