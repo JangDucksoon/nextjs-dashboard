0dafbd3178428f30e658d8bfeb9edc5b94357b54
jds <smaa11271@wavus.co.kr>
2025-06-23 13:30:08 +0900

    12장

--- app/ui/invoices/breadcrumbs.tsx
+++ app/ui/invoices/breadcrumbs.tsx
@@ -13,7 +13,10 @@ export default function Breadcrumbs({ breadcrumbs }: { breadcrumbs: Breadcrumb[]
 		<nav aria-label="Breadcrumb" className="mb-6 block">
 			<ol className={clsx(lusitana.className, 'flex text-xl md:text-2xl')}>
 				{breadcrumbs.map((breadcrumb, index) => (
-					<li key={breadcrumb.href} aria-current={breadcrumb.active} className={clsx(breadcrumb.active ? 'text-gray-900' : 'text-gray-500')}>
+					<li key={breadcrumb.href} aria-current={breadcrumb.active} className={clsx(
+						'hover:text-blue-500',
+						breadcrumb.active ? 'text-gray-900' : 'text-gray-500'
+					)}>
 						<Link href={breadcrumb.href}>{breadcrumb.label}</Link>
 						{index < breadcrumbs.length - 1 ? <span className="mx-3 inline-block">/</span> : null}
 					</li>
